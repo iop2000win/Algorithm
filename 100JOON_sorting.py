d661ec0e9f3987967f2c4521e618b670d7ea22b7
iop2000win <nowplerin@gmail.com>
2022-02-06 21:48:54 +0900

    Update Python File [100JOON_sorting.py]

--- 100JOON_sorting.py
+++ 100JOON_sorting.py
@@ -56,3 +56,85 @@ def count_sorting(input_list):
 		print(num)
     
 	return
+
+
+
+# Q 2108. 통계학
+'''
+패키지를 활용하지 않고 mode 값을 구하고 싶은데,
+시간 부분에서 계속 비효율적인 코드가 작성된다...
+'''
+
+# Q 11650. 좌표 정렬하기
+'''
+다중 조건을 이용한 정렬 방법을 익히는 예제
+'''
+
+def solution_Q11650(input_list):
+	input_list = input_list.copy()
+	input_list = sorted(input_list, key = lambda x: (x[0], x[1]))
+
+	for point in input_list:
+		print(point[0], point[1])
+
+	return
+
+# n = int(input())
+# input_list = [tuple(map(int, input().split())) for x in range(n)]
+
+
+# Q 1181. 단어 정렬
+'''
+중복 원소에 대한 처리를 익히는 예제 >>> set을 이용
+'''
+
+def solution_Q1181(input_list):
+	words_list = input_list.copy()
+	words_list = sorted(set(words_list), key = lambda x: (len(x), x))
+
+	for word in words_list:
+		print(word)
+
+	return
+
+# n = int(input())
+# input_list = [input() for x in range(n)]
+
+
+# Q 10814. 나이순 정렬
+
+def solution_Q10814(input_list):
+	member_list = sorted(member_list, key = lambda x: int(x[0]))
+
+	for member in member_list:
+		print(member[0], member[1])
+
+	return
+
+# n = int(input())
+# input_list = [tuple(input().split()) for x in range(n)]
+
+
+# Q 18870. 좌표 압축
+'''
+시간복잡도를 고려해야하는 문제
+list.index() <<< 메서드의 경우 시간복잡도 O(N)의 연산으로
+반복문 아래에서 사용할 경우 연산량을 급격하게 늘릴 수 있으므로
+사용에 유의해야한다.
+'''
+
+def solution_Q18870(input_list):
+	loc_list = input_list.copy()
+	num_list = sorted(set(loc_list))
+	num_dic = {}
+
+	for i in range(len(num_list)):
+		num_dic[num_list[i]] = i
+
+	for num in loc_list:
+		print(num_dic[num], end = ' ')
+
+	return
+
+# n = int(input())
+# input_list = list(map(int, input().split()))
