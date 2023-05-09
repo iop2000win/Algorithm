b7c464220c903bd419b0a1e89f8ef745444406e5
kdh_0823 <nowplerin@gmail.com>
2023-05-09 19:35:02 +0900

    Update .py file - 백준 큐 항목 문제 풀이 추가

--- 100JOON_queue.py
+++ 100JOON_queue.py
@@ -113,3 +113,102 @@ print('>', end = '')
 
 # -----------------------------------------------------------------------------------------------
 # Q 1966. 프린터 큐
+
+import sys
+from collections import deque
+
+def solution(input_list, target_index):
+    input_list = deque(input_list)
+    cnt = 0
+    
+    while True:
+        max_val = max(input_list)
+        
+        if input_list[0] == max_val:
+            input_list.popleft()
+            cnt += 1
+            
+            if target_index == 0:
+                return cnt
+            
+        else:
+            input_list.append(input_list.popleft())
+            
+        target_index -= 1
+        if target_index < 0:
+            target_index += len(input_list)
+            
+
+T = int(input())
+
+for t in range(T):
+    n, t = map(int, sys.stdin.readline().split())
+    input_list = list(map(int, sys.stdin.readline().split()))
+    
+    result = solution(input_list, t)
+    print(result)
+
+
+# -----------------------------------------------------------------------------------------------
+# Q 10866. 덱
+
+import sys
+from collections import deque
+
+def solution(input_str):
+    input_str = input_str.split()
+    global size
+
+    a = input_str[0]
+    
+    if a == 'push_front':
+        deq.appendleft(int(input_str[1]))
+        size += 1
+    elif a == 'push_back':
+        deq.append(int(input_str[1]))
+        size += 1
+    elif a == 'pop_front':
+        if not size == 0:
+            pop = deq.popleft()
+            size -= 1
+            print(pop)
+        else:
+            print(-1)
+    elif a == 'pop_back':
+        if not size == 0:
+            pop = deq.pop()
+            size -= 1
+            print(pop)
+        else:
+            print(-1)
+    elif a == 'size':
+        print(size)
+    elif a == 'empty':
+        if size == 0:
+            print(1)
+        else:
+            print(0)
+    elif a == 'front':
+        if not size == 0:
+            print(deq[0])
+        else:
+            print(-1)
+    elif a == 'back':
+        if not size == 0:
+            print(deq[-1])
+        else:
+            print(-1)
+    else:
+        print('올바른 키워드를 입력하세요')
+        
+    return
+
+
+T = int(input())
+
+deq = deque()
+size = 0
+
+for t in range(T):
+    input_str = sys.stdin.readline()
+    solution(input_str)
